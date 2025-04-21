fb7b32169e772a6700c21bc0874bd6e4f540629e
murilo <murilocolucci@gmail.com>
2025-04-21 15:13:48 -0300

    Criando exemplo de Record

---
+++ Programacao Orientada a Objetos/Abstracoes e Encapsulamentos/exemplo-record/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        var person = new Person("João");
+
+        System.out.println(person);
+        System.out.println(person.name());
+    }
+}
