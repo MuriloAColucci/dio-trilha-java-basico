1c9b833c0139ee411a0c4c5e29671a2d1d9fb3e2
murilo <murilocolucci@gmail.com>
2025-04-21 12:06:42 -0300

    Criando exemplo de Polimorfismo

---
+++ Programacao Orientada a Objetos/exemplo-polimorfismo/src/ComputadorPedrinho.java
@@ -0,0 +1,22 @@
+import apps.FacebookMessenger;
+import apps.MSNMessenger;
+import apps.ServicoMensagemInstantanea;
+import apps.TelegramMessenger;
+
+public class ComputadorPedrinho {
+    public static void main(String[] args) {
+        ServicoMensagemInstantanea smi = null;
+
+        String appEscolhido = "tlg";
+
+        if (appEscolhido.equals("msn"))
+            smi = new MSNMessenger();
+        else if(appEscolhido.equals("fbm"))
+            smi = new FacebookMessenger();
+        else if(appEscolhido.equals("tlg"))
+            smi = new TelegramMessenger();
+
+        smi.enviarMensagem();
+        smi.receberMensagem();
+    }
+}
