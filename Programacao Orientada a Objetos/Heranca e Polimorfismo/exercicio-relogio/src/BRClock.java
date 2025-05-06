43ac5f25123b4d98c1385d94787d9b0b294672a8
murilo <murilocolucci@gmail.com>
2025-05-06 10:52:09 -0300

    Criando exercicio relogio

---
+++ Programacao Orientada a Objetos/Heranca e Polimorfismo/exercicio-relogio/src/BRClock.java
@@ -0,0 +1,17 @@
+public non-sealed class BRClock extends Clock{
+
+    @Override
+    public Clock convert(final Clock clock){
+        this.second = clock.getSecond();
+        this.minute = clock.getMinute();
+
+        switch (clock){
+            case USClock usClock ->
+                this.hour = (usClock.getPeriodIndicator().equals("PM") ?
+                        usClock.getHour() + 12 : usClock.getHour());
+            case BRClock brClock ->
+                this.hour = brClock.getHour();
+        }
+        return this;
+    }
+}
---
+++ Programacao Orientada a Objetos/Heranca e Polimorfismo/exercicio-relogio/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Clock brclock = new BRClock();
+        brclock.setSecond(0);
+        brclock.setMinute(0);
+        brclock.setHour(25);
+
+        System.out.println(brclock.getTime());
+
+        System.out.println(new USClock().convert(brclock).getTime());
+    }
+}
