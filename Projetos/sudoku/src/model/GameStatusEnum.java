7ee9d09f6a58f28bfea942af85c7292aef07ae75
murilo <murilocolucci@gmail.com>
2025-05-09 11:48:37 -0300

    Criação do jogo Sudoku por terminal e por Tela

---
+++ Projetos/sudoku/src/model/GameStatusEnum.java
@@ -0,0 +1,17 @@
+package model;
+
+public enum GameStatusEnum {
+    NON_STARTED("Não iniciado"),
+    INCOMPLETE("Incompleto"),
+    COMPLETE("Completo");
+
+    private String label;
+
+    private GameStatusEnum(final String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+}
